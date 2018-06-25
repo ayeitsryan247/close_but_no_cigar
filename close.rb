36006228107d427b77b2646eb1fabc897867194b
ayeitsryan247 <39201054+ayeitsryan247@users.noreply.github.com>
2018-06-25 13:58:35 -0400

    compare and giving output

---
+++ close.rb
@@ -0,0 +1,19 @@
+def cigar(num,winnum)
+	num = num.split("")
+	winnum = winnum.split("")
+	counter = 0
+	num.each_with_index do |n, i| 
+		if n == winnum[i]
+			counter += 1
+		end
+	end
+	counter
+end
+
+def cigarr(mytickarray, winningtick)
+	ret = []
+mytickarray.each do |ticket|
+	 ret = cigar(ticket, winningtick)
+	 ret << cigar(ticket, winningtick)
+end	
+ 
--- close_test.rb
+++ close_test.rb
@@ -1,15 +1,24 @@
 require "minitest/autorun"
-require_relative "close"
+require_relative "close.rb"
 
-class Testcigar < Minitest::Test
+class TestCigar < Minitest::Test
 
     def test_assert_that_1_equals_1
-    assert_equal(1,1)
+        assert_equal(1, 1)
     end
 
+    def test_if_function_has_string
+    	assert_equal(Integer, cigar("", "").class)
+    end
 
+    def test_if_1_match_returns_1
+    	assert_equal(1, cigar("1234", "1555"))
+    end
 
-
-
-
+    def test_if_1_match_returns_2
+    	assert_equal(2,cigar("1234","1255"))
+    end
+    def test_if_array_returns_1
+    	assert_equal(1, cigararr(["1234", "1222", "12345"], "1234"))
+    end
 end
